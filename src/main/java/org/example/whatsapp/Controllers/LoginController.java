320d3894559213bab4eebf142df36e4360aa756a
richxrd7 <richardquintanapadron@gmail.com>
2025-01-09 18:19:24 +0000

    Hecha la lógica del login

--- src/main/java/org/example/whatsapp/Controllers/LoginController.java
+++ src/main/java/org/example/whatsapp/Controllers/LoginController.java
@@ -7,34 +7,19 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import org.example.whatsapp.Models.LoginModel;
 
 public class LoginController {
-
-    private final String user = "bet";
-    private final String pass = "1234";
-    private final String url = "";
-    Connection conexionLogin = null;
-
+    
     @FXML
     private ImageView imagenLogo;
 
     @FXML
-    private AnchorPane errorLabel;
-
-    @FXML
-    private Button buttonBorrar;
-
-    @FXML
-    private Button buttonIniciar;
+    private Label errorLabel;
 
     @FXML
     private TextField textFieldPass;
@@ -49,7 +34,22 @@ public class LoginController {
     }
 
     @FXML
-    void onClickIniciar(ActionEvent event) {
+    public void onClickLogin(ActionEvent event) {
+        String correo = textFieldUsuario.getText();
+        String password = textFieldPass.getText();
+
+        LoginModel model = new LoginModel();
+
+        boolean loginSucces = model.comprobarLogin(correo, password);
+
+        if (loginSucces){
+            goHome(event);
+        }else{
+            failedLogin();
+        }
+    }
+
+    public void goHome(ActionEvent event) {
         final String FXML = "/org/example/whatsapp/ContactosView.fxml";
 
         try {
@@ -58,87 +58,23 @@ public class LoginController {
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
             Scene scene =  new Scene(root);
+            stage.setScene(scene);
+
             stage.setHeight(800);
             stage.setWidth(1213);
-            stage.setScene(scene);
+
             stage.centerOnScreen();
             stage.show();
+
         }catch (Exception e){
             System.out.println(e);
         }
     }
 
-    void checkLogin(ActionEvent event) {
-        String usuario = textFieldUsuario.getText();
-        String password = textFieldPass.getText();
-
-        boolean existe = verificarLogin(usuario, password);
-
-        if (existe){
-            onClickIniciar(event);
-        }else{
-            loggedFail();
-        }
-    }
-
-    void loggedFail(){
+    public void failedLogin(){
+        errorLabel.setText("Correo o contraseña incorrecto, inténtelo de nuevo.");
         errorLabel.setStyle("-fx-text-fill: red");
     }
-
-
-
-
-
-    /** SE ESTABLECE LA CONECCION **/
-
-    public void startConnection(){
-        try{
-            conexionLogin = DriverManager.getConnection(url, user, pass);
-        }catch (Exception e){
-            System.out.println(e);
-        }
-    }
-
-    public void endConnection(){
-        try{
-            conexionLogin.close();
-            conexionLogin = null;
-        } catch (Exception e){
-            System.out.println(e);
-        }
-    }
-
-    public boolean verificarLogin(String user, String pass){
-        final String consulta = """
-                select nombre, apellido from alumnos
-                    where nombre = ? and apellido = ?; 
-               
-                """;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultados = null;
-
-        try{
-            startConnection();
-
-            preparedStatement = conexionLogin.prepareStatement(consulta);
-
-            preparedStatement.setString(1, user);
-            preparedStatement.setString(2, pass);
-
-            resultados = preparedStatement.executeQuery();
-
-            if (resultados.next()){
-                return true;
-            }
-        } catch (Exception e){
-            System.out.println(e);
-        } finally {
-            endConnection();
-        }
-        return false;
-    }
-
-
 
 
 }
