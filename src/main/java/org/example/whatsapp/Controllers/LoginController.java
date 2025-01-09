3bbe66bbe5b80fc687a3b833b5daa9aeb8f6ac43
richxrd7 <richardquintanapadron@gmail.com>
2025-01-09 19:29:18 +0000

    Hecho el login para que le haga la petición al servidor :D

--- src/main/java/org/example/whatsapp/Controllers/LoginController.java
+++ src/main/java/org/example/whatsapp/Controllers/LoginController.java
@@ -6,13 +6,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import org.example.whatsapp.Models.LoginModel;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginController {
 
@@ -33,9 +37,7 @@ public class LoginController {
         String correo = textFieldUsuario.getText();
         String password = textFieldPass.getText();
 
-        LoginModel model = new LoginModel();
-
-        boolean loginSucces = model.comprobarLogin(correo, password);
+        boolean loginSucces = loginServidor(correo, password);
 
         if (loginSucces){
             goHome(event);
@@ -93,6 +95,38 @@ public class LoginController {
         errorLabel.setVisible(true);
         errorLabel.setText("Correo o contraseña incorrecto, inténtelo de nuevo.");
         errorLabel.setStyle("-fx-text-fill: red");
+    }
+
+    public boolean loginServidor(String correo, String pass){
+        final String HOST = "localhost";
+        final int PORT = 5000;
+
+        try (Socket socket = new Socket(HOST, PORT)){
+            //Ponemos los datos en un Map
+            Map<String, String> datosLogin = new HashMap<>();
+            datosLogin.put("correo", correo);
+            datosLogin.put("password", pass);
+
+            ObjectOutputStream salida = new ObjectOutputStream(socket.getOutputStream());
+            salida.writeObject(datosLogin);
+            salida.flush();
+
+            //Esperamos la respuesta del servidor
+            ObjectInputStream entrada = new ObjectInputStream(socket.getInputStream());
+            String respuesta = (String) entrada.readObject();
+
+            if (respuesta.equals("true")){
+                System.out.println("Se ha logeado correctamente");
+                return true;
+            } else{
+                System.out.println("No se ha podido logear");
+                return false;
+            }
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+        return false;
     }
 
 
---
+++ src/main/java/org/example/whatsapp/Objects/Mensaje.java
@@ -0,0 +1,52 @@
+package org.example.whatsapp.Objects;
+
+public class Mensaje {
+
+    private int id;
+    private Usuario remitente;
+    private Usuario destinatario;
+    private String mensaje;
+    private String fecha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Usuario getRemitente() {
+        return remitente;
+    }
+
+    public void setRemitente(Usuario remitente) {
+        this.remitente = remitente;
+    }
+
+    public Usuario getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(Usuario destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+}
+
