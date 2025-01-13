2535873e87011356c65ddf0f3a569c985724c43e
Betsaidars <betsaidars97@gmail.com>
2025-01-13 11:39:00 +0000

    Creados los bocadillos de los chats

---
+++ src/main/java/org/example/whatsapp/Controllers/ChatEnviadoController.java
@@ -0,0 +1,14 @@
+package org.example.whatsapp.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ChatEnviadoController {
+
+    @FXML
+    private Label horaEnviadoLabel;
+
+    @FXML
+    private Label mensajeEnviadoLabel;
+
+}
---
+++ src/main/java/org/example/whatsapp/Controllers/ChatRecibidoController.java
@@ -0,0 +1,17 @@
+package org.example.whatsapp.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ChatRecibidoController {
+
+    @FXML
+    private Label chatRecibidoLabel;
+
+    @FXML
+    private Label horaRecibidoLabel;
+
+    @FXML
+    private Label mensajeRecibidoLabel;
+
+}
